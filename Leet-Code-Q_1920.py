3d6d12e7b381bc1ab34782924c3188b04a8d3f3f
Ahad Ali <ranaahad9095@gmail.com>
2024-10-13 20:45:10 -0700

    Solving Leet_Code Question no. 1920

---
+++ Leet-Code-Q_1920.py
@@ -0,0 +1,18 @@
+""""
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+"""
+
+# Create a list with the name 'nums'
+nums = [5,3,1,0,2,4]
+
+# Create a list with name 'ans' size as nums
+ans = [0]*len(nums)
+
+# Fill ans according to the problem's requirement
+for i in range(len(nums)):
+    ans[i] = nums[nums[i]]
+
+#printing the answer 
+print(ans) 
