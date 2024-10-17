75c37c122e4ef1942296684dac4a49e4bc2504cc
Ahad Ali <ranaahad9095@gmail.com>
2024-10-16 18:41:27 -0700

    Solving Leet_Code Question no. 28 using .find()

---
+++ Leet-Code-Q_28.py
@@ -0,0 +1,12 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+"""
+
+haystack = 'sadbutsad'
+needle = 'but'
+
+# Find the index of the first occurrence of 'needle' in 'haystack'
+index = haystack.find(needle)
+
+
+print("index : ", index)
