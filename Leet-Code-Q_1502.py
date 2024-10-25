1d900ee8d1b40253ecd861e574754a2148678ff4
Ahad Ali <ranaahad9095@gmail.com>
2024-10-24 18:05:17 -0700

    Completing Leet_Code Question no. 1502

--- Leet-Code-Q_1502.py
+++ Leet-Code-Q_1502.py
@@ -5,10 +5,19 @@ Given an array of numbers arr, return true if the array can be rearranged to for
 """
 
 arr = [-13,-17,-8,-10,-20,2,3,-19,2,-18,-5,7,-12,18,-17,12,-1]
+# arr = [3,5,1]
 sort = sorted(arr)
 print(sort)
-i = 0
-if (sort[i]-sort[i+1]) == (sort[i+1]-sort[i+2]):
+
+difference = sort[1] - sort[0]
+is_arithmetic = True
+
+for i in range(2, len(sort)):
+    if ((sort[i] - sort[i-1]) != difference):
+        is_arithmetic = False
+        break
+
+if is_arithmetic:
     print("True")
 else:
     print("False")
