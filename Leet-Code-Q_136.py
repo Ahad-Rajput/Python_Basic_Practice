fe1b32691180fd67941681e7f962470d820037a5
Ahad Ali <ranaahad9095@gmail.com>
2024-12-26 12:55:02 -0800

    Solving Leet-Code Question 136

---
+++ Leet-Code-Q_136.py
@@ -0,0 +1,14 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space
+"""
+
+arr = []
+nums = [2,1,4,2,1]
+for num in nums:
+    if num not in arr:
+        arr.append(num)
+    else:
+        arr.remove(num)
+print(arr.pop())
