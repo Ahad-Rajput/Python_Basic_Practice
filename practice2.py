9c0bc72dbdbed7b5c0769b32f9be6a6e4624b3be
Ahad Ali <ranaahad9095@gmail.com>
2025-02-11 21:58:15 -0800

    Completing binary search algorithm in python.

--- practice2.py
+++ practice2.py
@@ -1,23 +1,24 @@
-def binary_search(list, target):
-    found = False
+def binary_search(arr, target):
+    
     start = 0
-    end = len(list) + 1 
-    mid = int((start + end) / 2)
-    for i in list:
-        if target == list[mid]:
-            found = True
-            print(f"{target} is found.")
-            break
-        elif target > list[mid]:
+    end = len(arr) - 1
+
+    while start <= end:
+        mid = int(start+end / 2 )
+
+        if target == arr[mid]:
+            print(f"{target} is present at {mid} index.")
+            return
+        elif target > arr[mid]:
             start = mid + 1
-        elif target < list[mid]:
-            end = mid - 1
-        mid = int((start + end) / 2)
-    if found != True:
-        print(f"{target} is not found.")
+        else:
+            end = mid - 1 
+    print(f"{target} is not present in array.")
 
-num = int(input("Enter the number, You want to find : "))
 
-list = [1,2,3,4,5,6,7,8,9]
 
-binary_search(list, num)
+arr = [2, 4, 6, 8, 10, 12, 14, 16]
+
+num = int(input("Enter number, you want to find : ")) 
+
+binary_search(arr, num)
