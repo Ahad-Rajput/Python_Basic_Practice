17a19174b1b708efcced83eddea120a0013b17a3
Ahad Ali <ranaahad9095@gmail.com>
2025-03-03 21:29:34 -0800

    Practicing , Python Simple programs..

---
+++ Simple things/checking_in_string.py
@@ -0,0 +1,8 @@
+txt1 = "Quaid-e-Azam Muhammad Ali Jinnah was the founder of Pakistan."
+
+if "Ali" in txt1:
+    print("Yes,'Ali' is present in txt.")
+
+
+txt2 = "The best things in life are free!"
+print("expensive" not in txt2)
